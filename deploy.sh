b030929e5d9ebd7ca579f5d884d378f81934cba0
Jakub.Pazdyga <Jakub.Pazdyga@ft.com>
2015-07-21 13:46:11 +0100

    Added logs volume

--- deploy.sh
+++ deploy.sh
@@ -69,7 +69,7 @@ dockerdirs() {
 		target=`echo $configfile | cut -d/ -f4- | sed 's|^|/etc\/|g'`
 		echo "ADD $configfile $target" >> Dockerfile
 	done
-	echo -e "VOLUME $wwwpath2sub/\nENV DATE_TIMEZONE UTC\nEXPOSE 80 443\nUSER root\nCMD [\"/usr/bin/supervisord\", \"-n\", \"-c/etc/supervisor.d/supervisord.conf\"]" >> Dockerfile
+	echo -e "VOLUME $wwwpath2sub /var/log\nENV DATE_TIMEZONE UTC\nEXPOSE 80 443\nUSER root\nCMD [\"/usr/bin/supervisord\", \"-n\", \"-c/etc/supervisor.d/supervisord.conf\"]" >> Dockerfile
 
 }
 
